167744eef4a49c1bb7e7a6169478467ef7c8d167
luizriato <luiz.riato@outlook.com>
2024-10-29 13:48:34 -0300

    Update Routes.js

--- ADA COMPANY/MVC/Frontend/src/routes/Routes.js
+++ ADA COMPANY/MVC/Frontend/src/routes/Routes.js
@@ -1,10 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
+import { Projects } from "./components/Projects/Projects";
+import SignIn from "./components/sign-in/SignIn"
+import SignUpClient from "./components/sign-up-client/SignUpClient"
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hero />} />
+      <Route path="/" element={<Hero/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/signupclient" element={<SignUpClient />} />
       </Routes>
     </Router>
   );
