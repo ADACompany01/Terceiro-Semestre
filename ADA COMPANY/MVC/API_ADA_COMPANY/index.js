2d8025179d4cbcb2712f588a962a4da92125ab90
luizriato <luiz.riato@outlook.com>
2024-10-29 17:25:15 -0300

    update

--- ADA COMPANY/MVC/API_ADA_COMPANY/index.js
+++ ADA COMPANY/MVC/API_ADA_COMPANY/index.js
@@ -4,6 +4,17 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
+// Middleware para habilitar CORS para todas as origens específicas
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://adacompany.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);  // Responde rápido para preflight requests
+    }
+    next();
+});
+
 // Importando rotas
 const clienteRoute = require('./src/routes/clienteRoute');
 const servicoRoute = require('./src/routes/servicoRoute');
