6563b87e55b09ebea9a420b23397791a5d5e784d
luizriato <luiz.riato@outlook.com>
2024-10-29 14:05:03 -0300

    Update Routes.js

--- ADA COMPANY/MVC/Frontend/src/routes/Routes.js
+++ ADA COMPANY/MVC/Frontend/src/routes/Routes.js
@@ -5,7 +5,7 @@ import { Hero } from "./components/Hero/Hero";
 import { Projects } from "./components/Projects/Projects";
 import SignIn from "./components/sign-in/SignIn"
 import SignUpClient from "./components/sign-up-client/SignUpClient"
-import SignUpServico from "./components/sign-up-SignUpServico/SignUpServico"
+import SignUpServico from "./components/sign-up-servico/SignUpServico"; 
 function AppRoutes() {
   return (
     <Router>
