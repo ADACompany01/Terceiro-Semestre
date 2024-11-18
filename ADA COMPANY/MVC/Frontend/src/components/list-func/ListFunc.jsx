248643c4ab642722b957a09ecb9375d3dac2275b
luizriato <luiz.riato@outlook.com>
2024-11-17 23:37:56 -0300

    fix: consulta
    correção de consulta de funcionario

--- ADA COMPANY/MVC/Frontend/src/components/list-func/ListFunc.jsx
+++ ADA COMPANY/MVC/Frontend/src/components/list-func/ListFunc.jsx
@@ -14,6 +14,7 @@ import { styled } from '@mui/material/styles';
 import AppTheme from '../shared-theme/AppTheme';
 import { GoogleIcon, FacebookIcon, SitemarkIcon } from './CustomIcons';
 import ColorModeSelect from '../shared-theme/ColorModeSelect';
+import { Navigate } from 'react-router-dom';
 
 const Card = styled(MuiCard)(({ theme }) => ({
   display: 'flex',
@@ -49,9 +50,14 @@ export default function ListFunc(props) {
   }
   const handleConsultaCliente = async () => {
     try {
-      const response = await fetch(`https://api-ada-company.vercel.app/cliente/${funcionarioId}`);
+      const response = await fetch(`https://api-ada-company.vercel.app/funcionarios/${funcionarioId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       if (!response.ok) {
-        throw new Error('Funcionário não encontrado');
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Erro ao buscar funcionário');
       }
       const data = await response.json();
       setfuncionarioData(data);
