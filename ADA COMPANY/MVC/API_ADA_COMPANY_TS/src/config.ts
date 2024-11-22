402fbaea0456611694962479c4b41d7fcbc66dfd
MatheusPrusch77 <julio.sillig68@gmail.com>
2024-11-22 14:24:26 -0300

    refactor: secret key
    retirado o secret key do arquivo config.ts para o .env por questões de segurança

--- ADA COMPANY/MVC/API_ADA_COMPANY_TS/src/config.ts
+++ ADA COMPANY/MVC/API_ADA_COMPANY_TS/src/config.ts
@@ -4,13 +4,18 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 interface Config {
+  MONGODB_URI: string,
   JWT_SECRET: string;
   // Outras variáveis de configuração, se necessário
 }
 
 const config: Config = {
-  JWT_SECRET: process.env.JWT_SECRET || 'ADACOMPANY', // Use uma chave segura
-  // Outras variáveis de configuração...
+  MONGODB_URI: process.env.MONGODB_URI || '', //Garantir que as variáveis existam no .env
+  JWT_SECRET: process.env.JWT_SECRET || '',  
 };
+
+if(!config.MONGODB_URI || !config.JWT_SECRET) {
+  throw new Error('Faltam variáveis de ambiente essencias no arquivo .env');
+}
 
 export default config;
