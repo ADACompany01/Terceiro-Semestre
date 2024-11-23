8d64585d819e9f216f5c89168b70234d364b85ae
luizriato <luiz.riato@outlook.com>
2024-11-23 19:15:21 -0300

    fix: acesso do cliente
    alteração da área de acesso do cliente

--- ADA COMPANY/MVC/Frontend/src/components/client-access/client-access.jsx
+++ ADA COMPANY/MVC/Frontend/src/components/client-access/client-access.jsx
@@ -8,13 +8,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import ColorModeSelect from '../shared-theme/ColorModeSelect';
 import MuiCard from '@mui/material/Card';
 import { styled } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
-import TextField from '@mui/material/TextField';
-import { jwtDecode } from 'jwt-decode';
 
 
 
@@ -44,132 +38,28 @@ const SignUpContainer = styled(Stack)(({ theme }) => ({
 
 export const AcessoCliente = () => {
   const token = localStorage.getItem('token');
-  const [clienteData, setClienteData] = React.useState(null);
 
   if (!token) {
     return <Navigate to="/signin" />;
   }
 
-  const decodedToken = jwtDecode(token);
-  const userRole = decodedToken.role;// Assuming the user ID is stored as 'id' in the token
-  localStorage.setItem('userRole', userRole);
-
-  const formatValue = (value) => {
-    if (Array.isArray(value)) {
-      return value.join(', ');
-    }
-    if (typeof value === 'object' && value !== null) {
-      return JSON.stringify(value);
-    }
-    return value || '';
-  };
-
-  const handleConsultaCliente = async () => {
-    try {
-      const response = await fetch(`https://api-ada-company.vercel.app/cliente/${userRole}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message || 'Erro ao buscar cliente');
-      }
-      const data = await response.json();
-      setClienteData(data);
-    } catch (error) {
-      console.error('Erro ao buscar cliente:', error);
-      setClienteData(null);
-    }
-  };
-
-  React.useEffect(() => {
-    handleConsultaCliente();
-  }, []); // Call handleConsultaCliente once on component mount
-
-
   return (
-    <AppTheme>
-      <CssBaseline enableColorScheme />
-      <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
-      <SignUpContainer direction="column" justifyContent="space-between">
-        <Card variant="outlined">
-          <SitemarkIcon />
-          <Box sx={{ display: 'flex', gap: 2 }}>
-            <Button variant="contained" onClick={handleConsultaCliente}>
-              Meus dados
-            </Button>
-          </Box>
-          {clienteData && (
-            <Box sx={{ mt: 2 }}>
-              <Typography variant="h6">Dados do Cliente:</Typography>
-              <TextField
-                label="Telefone"
-                fullWidth
-                value={formatValue(clienteData.telefone)}
-                disabled
-              />
-              <TextField
-                label="CEP"
-                fullWidth
-                value={formatValue(clienteData.endereco?.cep)}
-                disabled
-              />
-              <TextField
-                label="Logradouro"
-                fullWidth
-                value={formatValue(clienteData.endereco?.logradouro)}
-                disabled
-              />
-              <TextField
-                label="Bairro"
-                fullWidth
-                value={formatValue(clienteData.endereco?.bairro)}
-                disabled
-              />
-              <TextField
-                label="UF"
-                fullWidth
-                value={formatValue(clienteData.endereco?.uf)}
-                disabled
-              />
-              <TextField
-                label="Estado"
-                fullWidth
-                value={formatValue(clienteData.endereco?.estado)}
-                disabled
-              />
-              <TextField
-                label="DDD"
-                fullWidth
-                value={formatValue(clienteData.endereco?.ddd)}
-                disabled
-              />
-              <TextField
-                label="Email"
-                fullWidth
-                value={formatValue(clienteData.usuario?.email)}
-                disabled
-              />
-              <TextField
-                label="Telefone do Usuário"
-                fullWidth
-                value={formatValue(clienteData.usuario?.telefone)}
-                disabled
-              />
-              <TextField
-                label="Nome Completo do Usuário"
-                fullWidth
-                value={formatValue(clienteData.usuario?.nomeCompleto)}
-                disabled
-              />
-            </Box>
-          )}
-        </Card>
-        <Divider>
-          <Typography sx={{ color: 'text.secondary' }}>ou</Typography>
-        </Divider>
-      </SignUpContainer>
-      </AppTheme>
+    <section className={styles.container} id="experience">
+    <h2 className={styles.title}>Usuário</h2>
+    <div className={styles.content}>
+      <div className={styles.skills}>
+        {skills.map((skill, id) => {
+          return (
+            <Link to={skill.link} key={id} className={`${styles.skill} ${styles.link}`}>
+              <div className={styles.skillImageContainer}>
+                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+              </div>
+              <p>{skill.title}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  </section>
   );
 };
