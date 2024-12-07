b7d19ea8501f0a5c9b1c5ecce755439088509030
ADACompany01 <ada2024fatec.mrs@outlook.com>
2024-12-07 10:13:58 -0300

    feat: hotjar
    adição de hotjar ao navbar

--- ADA COMPANY/MVC/Frontend/src/components/Navbar/Navbar.jsx
+++ ADA COMPANY/MVC/Frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";  
-
+import { jwtDecode } from "jwt-decode";
 
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
@@ -10,26 +9,43 @@ export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
-  const token = localStorage.getItem('token');
-  const user = JSON.parse(localStorage.getItem('user'));
-  
+  const token = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    // Código do Hotjar
+    (function (h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function () {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 5226215, hjsv: 6 };
+      a = o.getElementsByTagName("head")[0];
+      r = o.createElement("script");
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+  }, []); // O useEffect será executado apenas uma vez após o primeiro render do componente
+
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/signin');
+    localStorage.removeItem("token");
+    navigate("/signin");
   };
 
   const handleMenu = () => {
     const decodedToken = jwtDecode(token);
     const userRole = decodedToken?.role; // Handle potential null value
-    if (userRole === 'cliente') {
-      navigate('/client');
-    } else if (userRole === 'admin') {
-      navigate('/admin');
+    if (userRole === "cliente") {
+      navigate("/client");
+    } else if (userRole === "admin") {
+      navigate("/admin");
     }
-  }
+  };
 
   const handleNavigateToSection = (sectionId) => {
-    if (location.pathname === '/' || location.pathname === '/signin') {
+    if (location.pathname === "/" || location.pathname === "/signin") {
       handleScrollToSection(sectionId);
     } else {
       navigate(`/${sectionId}`);
@@ -42,6 +58,7 @@ export const Navbar = () => {
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
+
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
@@ -71,10 +88,10 @@ export const Navbar = () => {
           {token ? (
             <>
               <li>
-                <a onClick={handleLogout} >Logout</a>
+                <a onClick={handleLogout}>Logout</a>
               </li>
               <li>
-              <a onClick={handleMenu} >Menu</a>
+                <a onClick={handleMenu}>Menu</a>
               </li>
             </>
           ) : (
